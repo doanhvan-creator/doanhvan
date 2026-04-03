a3b4f5ae376653d4da875cfaaf75ecdfb034deca
doanhvan-creator <van_2451220089@dau.edu.vn>
2026-04-03 15:23:29 +0700

    Time: 4 ms (5.52%), Space: 14.2 MB (79.4%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(), cost.end(), greater<int>());
+        
+        int total = 0;
+        
+        for (int i = 0; i < cost.size(); i++) {
+            // bỏ qua mỗi viên thứ 3 (free)
+            if (i % 3 != 2) {
+                total += cost[i];
+            }
+        }
+        
+        return total;
+    }
+};

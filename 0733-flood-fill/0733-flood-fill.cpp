329bb940b0ec9f3e0ffc7c5a93c5ecbb8e4d6a62
doanhvan-creator <van_2451220089@dau.edu.vn>
2026-05-15 09:25:36 +0700

    Time: 0 ms (100%), Space: 18.1 MB (74.53%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& image, int r, int c,
+             int oldColor, int newColor) {
+
+        int m = image.size();
+        int n = image[0].size();
+
+        // Check boundaries
+        if (r < 0 || c < 0 || r >= m || c >= n)
+            return;
+
+        // Stop if color is different
+        if (image[r][c] != oldColor)
+            return;
+
+        // Change color
+        image[r][c] = newColor;
+
+        // Visit 4 directions
+        dfs(image, r + 1, c, oldColor, newColor);
+        dfs(image, r - 1, c, oldColor, newColor);
+        dfs(image, r, c + 1, oldColor, newColor);
+        dfs(image, r, c - 1, oldColor, newColor);
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image,
+                                  int sr, int sc, int color) {
+
+        int oldColor = image[sr][sc];
+
+        // No need to fill if colors are same
+        if (oldColor == color)
+            return image;
+
+        dfs(image, sr, sc, oldColor, color);
+
+        return image;
+    }
+};

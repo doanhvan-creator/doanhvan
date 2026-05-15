a23acf4a6c8fe3ab96adb490dfe6926d6470ea3f
doanhvan-creator <van_2451220089@dau.edu.vn>
2026-05-15 09:22:43 +0700

    Time: 0 ms (100%), Space: 101.9 MB (84.48%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        int perimeter = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                if (grid[i][j] == 1) {
+
+                    // Each land cell contributes 4 sides
+                    perimeter += 4;
+
+                    // Remove shared edges
+                    if (i > 0 && grid[i - 1][j] == 1)
+                        perimeter -= 2;
+
+                    if (j > 0 && grid[i][j - 1] == 1)
+                        perimeter -= 2;
+                }
+            }
+        }
+
+        return perimeter;
+    }
+};
